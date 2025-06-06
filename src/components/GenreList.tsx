09dfa5bec81995c3a4d294de76be4621bda0a2dd
Ondrej <536477@muni.cz>
2025-06-06 07:18:32 +0200

    style: lineup the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,38 +1,68 @@
-import { HStack, List, ListItem, Image, Text, Spinner, Box, Button } from '@chakra-ui/react';
-import useGenres from '../hooks/useGenres';
-import getCroppedImageUrl from '../services/image-url';
-import { Genre } from '../hooks/useGenres';
+import {
+  HStack,
+  List,
+  ListItem,
+  Image,
+  Spinner,
+  Box,
+  Button,
+  Heading,
+} from "@chakra-ui/react";
+import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
+import { Genre } from "../hooks/useGenres";
 
-interface Props{
-    onSelectGenre: (genre: Genre) => void;
-    selectedGenre: Genre | null;
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
-    const {data, isLoading, error} = useGenres();
+const GenreList = ({ onSelectGenre, selectedGenre }: Props) => {
+  const { data, isLoading, error } = useGenres();
 
-    if (error) return null;
+  if (error) return null;
 
-    if (isLoading) return (
-        <Box display="flex" justifyContent="center" alignItems="center" height="100%">
-            <Spinner />
-        </Box>
-    )
-
+  if (isLoading)
     return (
-        <List> 
-            {data.map((genre) => (
-                <ListItem key={genre.id} paddingY="5px">
-                    <HStack>
-                        <Image src={getCroppedImageUrl(genre.image_background)} boxSize="32px" borderRadius={8} objectFit="cover"/>
-                        <Button fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"} variant="link" onClick={() => onSelectGenre(genre)}>
-                            {genre.name}
-                        </Button>
-                    </HStack>
-                </ListItem>
-            ))}
-        </List>
-    )
-}
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100%"
+      >
+        <Spinner />
+      </Box>
+    );
+
+  return (
+    <>
+      <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
+
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                objectFit="cover"
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+              />
+              <Button
+                whiteSpace={"normal"}
+                textAlign={"left"}
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
+                variant="link"
+                onClick={() => onSelectGenre(genre)}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
+  );
+};
 
 export default GenreList;
